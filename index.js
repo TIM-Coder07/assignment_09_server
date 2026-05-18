db90507cc05b6ef50aa172c5cdd26b559dcfc2a4
TIM-Coder07 <sajonwdd72566@gmail.com>
2026-05-18 07:36:20 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+const app = express()
+const port = process.env.PORT || 8000;
+const uri = process.env.MONGO_DB_URI
+
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
